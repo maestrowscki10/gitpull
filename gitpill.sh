b241d1e4fa6ba2b92d5dd01ecace07f21bbb89a9
maestrowscki10 <abdulhansen.ah@gmail.com>
2022-04-29 21:21:22 +0300

    git pull

---
+++ gitpill.sh
@@ -0,0 +1,8 @@
+#fetch the remote content
+git pull <remote>
+#fetch remote content but not create merge
+git pull --no-commit <remote>
+#intergrate the remote branch
+git pull --rebase <remote>
+#show download content and merge details
+git pull --verbose
